5914b1f9aa054ec1162d23783c79dde9a5673261
santosh <bsantosh@thoughtworks.com>
2020-02-17 23:56:25 +0530

    Adds steps to install mysql locally

--- ALB/ansible/templates/wp-config.php
+++ ALB/ansible/templates/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', '{{ wp_db_name }}');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', '{{ wp_db_user }}');
+define('DB_USER', 'wp_user');
 
 /** MySQL database password */
 define('DB_PASSWORD', '{{ wp_db_password }}');
 
 /** MySQL hostname */
-define('DB_HOST', '{{ wp_db_host}}');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
